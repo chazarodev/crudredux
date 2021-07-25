73aace4ed372bd73b26510a9ddf2181e58cdab49
Alberto Cházaro <shazarerom@gmail.com>
2021-07-25 13:51:06 -0500

    REDUX: Primeros pasos

---
+++ src/components/NuevoProducto.jsx
@@ -0,0 +1,78 @@
+import React, {useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+//Actions de Redux
+import {crearNuevoProductoAction} from '../actions/productoActions'; 
+
+const NuevoProducto = () => {
+
+    //state del componente
+    const [nombre, guardarNombre] = useState('');
+    const [precio, guardarPrecio] = useState(0);
+
+    //Utilizar useDispatch y te crea una función
+    const dispatch = useDispatch();
+
+    //Manda a llamar el action de productoAction
+    const agregarProducto = (producto) => dispatch(crearNuevoProductoAction(producto));
+
+    //Cuando el usuario haga submit
+    const submitNuevoProducto = e => {
+
+        e.preventDefault();
+
+        //Validar formulario
+        if (nombre.trim() === '' || precio <= 0) {
+            return;
+        };
+
+        //si no hay errores
+
+        //Crear nuevo proudcto
+        agregarProducto({
+            nombre,
+            precio
+        });
+    };
+
+    return (  
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">Agregar Nuevo Producto</h2>
+                        <form onSubmit={submitNuevoProducto}>
+                            <div className="form-group">
+                                <label>Nombre Producto</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control"
+                                    placeholder="Nombre Producto"
+                                    name="Nombre" 
+                                    value={nombre}
+                                    onChange={e => guardarNombre(e.target.value)}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Precio Producto</label>
+                                <input 
+                                    type="number" 
+                                    className="form-control"
+                                    placeholder="Precio Producto"
+                                    name="Precio"
+                                    value={precio}
+                                    onChange={e => guardarPrecio(Number(e.target.value))}
+                                />
+                            </div>
+                            <button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100">
+                                Agregar 
+                            </button>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default NuevoProducto;
